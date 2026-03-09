d864bd1e4f365a15dd1268d5e8c1e5855bb4a28b
hsk-2004 <harman.singh.23cse@bmu.edu.in>
2026-03-09 10:05:57 +0530

    message

--- components/sections/explore/FounderLetter.tsx
+++ components/sections/explore/FounderLetter.tsx
@@ -4,17 +4,15 @@ import Image from "next/image";
 
 export default function FounderLetter() {
     return (
-        <section className="bg-[#F0EEE6]">
-            {/* Full-width container with no horizontal padding */}
-            <div className="w-full relative aspect-[428/827]">
-                <Image
-                    src="/explore/f1.svg"
-                    alt="A letter from the Founder"
-                    fill
-                    className="object-contain"
-                    priority
-                />
-            </div>
+        <section className="bg-[#F0EEE6] flex justify-center">
+            <Image
+                src="/explore/f1.svg"
+                alt="A letter from the Founder"
+                width={428}
+                height={827}
+                priority
+                unoptimized
+            />
         </section>
     );
 }
