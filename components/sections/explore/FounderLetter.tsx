07ea113d41a8bf745da927abb24b2578401bdcf3
hsk-2004 <harman.singh.23cse@bmu.edu.in>
2026-03-09 09:48:40 +0530

    quality issue

--- components/sections/explore/FounderLetter.tsx
+++ components/sections/explore/FounderLetter.tsx
@@ -6,12 +6,12 @@ export default function FounderLetter() {
     return (
         <section className="bg-[#F0EEE6]">
             {/* Full-width container with no horizontal padding */}
-            <div className="w-full relative aspect-[1/2.1] md:aspect-[1/1.5] lg:aspect-[1/1.2]">
+            <div className="w-full relative aspect-[428/827]">
                 <Image
                     src="/explore/f1.svg"
                     alt="A letter from the Founder"
                     fill
-                    className="object-contain md:object-cover"
+                    className="object-contain"
                     priority
                 />
             </div>
